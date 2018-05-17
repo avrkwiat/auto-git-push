8da8d3d5bb9347b458313c81a8ce2ee830484f94
Marcin Kwiat <35290353+avrkwiat@users.noreply.github.com>
2018-05-17 16:25:48 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,9 @@
 
 void setup() {
   Serial.begin(9600);
-    // put your setup code here, to run once:
 }
 
 void loop() {
   Serial.printf("Test ssh");
   delay(3000);
-  //test po dodaniu klucza ssh
 }
